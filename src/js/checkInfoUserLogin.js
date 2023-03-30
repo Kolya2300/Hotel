4e9cddfb0cbf117bdb4ff273ef89ac436483a2ca
Kolya <nk6476066@gmail.com>
2023-03-30 21:24:14 +0300

    user login, but register don't work register

--- src/js/checkInfoUserLogin.js
+++ src/js/checkInfoUserLogin.js
@@ -1,61 +1,51 @@
 const checkInfoUserLogin = document.querySelector('.checkInfoUserLogin');
 
-checkInfoUserLogin.addEventListener('submit', (event)=>{
+checkInfoUserLogin.addEventListener('submit', (event) => {
     event.preventDefault()
     const login = checkInfoUserLogin.elements.login.value;
     const password = checkInfoUserLogin.elements.password.value;
     if (login === 'admin@gmail.com' && password === 'admin') {
         window.location.href = 'adminPage/Show.html';
     } else {
-        const registerForm = document.querySelector('.register__form')
-
-
-
-        registerForm.addEventListener('submit', async (event) => {
+        const loginForm = document.querySelector('.login_form')
+        loginForm.addEventListener('submit', async (event) => {
             event.preventDefault()
-            const registerFormUserName = document.querySelector('.register__form-user-name').value;
-            const registerFormEmail = document.querySelector('.register__form-email').value;
-            const registerFormUserPhone = document.querySelector('.register__form-tel').value;
-            const registerFormUserPassword = document.querySelector('.register__form-password').value;
-            const registerFormUserChecked = document.querySelector('.register__form-checked').value;
-
+            const email = document.querySelector('.login__form-email').value;
+            const password = document.querySelector('.login__form-password').value;
+            const response = await fetch("http://localhost:3000/users?email=" + email)
+            const users = await response.json()
+            if (users.length > 0 && users[0].password === password) {
+                localStorage.setItem('user', JSON.stringify(users[0]));
+                window.location.href = './profileUser.html';
+            } else {
+                alert('Invalid email or password')
+            }
+        })
+        const registerForm = document.querySelector('.register__form')
+        registerForm.addEventListener("submit", async (event) => {
+            event.preventDefault();
+            const name = document.querySelector('.register__form-user-name').value;
+            const email = document.querySelector('.register__form-email').value;
+            const phone = document.querySelector('.register__form-tel').value;
+            const password = document.querySelector('.register__form-password').value;
+            const newUser = {name, email, phone, password};
             /*fetch data, adding to db.json */
-            await fetch("http://localhost:3000/users",{
-                method: "POST",
-                headers: {
-                    "Content-Type" : "application/json"
-                },
-                body: JSON.stringify({registerFormUserName, registerFormEmail, registerFormUserPhone, registerFormUserPassword, registerFormUserChecked})
+            await fetch("http://localhost:3000/users", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(newUser),
             })
                 .then(response =>{
-                    if(response.ok){
-                        alert('Registration succesful')
-                        registerForm.reset()
-                    }else{
-                        alert('Some error')
-                    }
-                })
-                .catch(err => console.error(err))
-        })
+                if (response.ok) {
+                    console.log('some')
+                    localStorage.setItem('user', JSON.stringify(newUser));
+                    window.location.href = './profileUser.html';
+                } else {
+                    alert('Registration failed.');
+                }
+            })
+                .catch(error => console.error(error))
 
-
-        const loginForm = document.querySelector('.login__form')
-        loginForm.addEventListener('submit', async (event)=>{
-            event.preventDefault()
-            const loginFormEmail = document.querySelector('.login__form-email').value;
-            const loginFormPassword = document.querySelector('.login__form-password').value;
-            const response = await fetch("http://localhost:3000/users")
-            const users = await response.json()
-            const user = users.find(user => user.registerFormEmail === loginFormEmail && user.registerFormUserPassword === loginFormPassword)
-            if(user){
-                alert('Login successful')
-                localStorage.setItem('loginFormEmail', loginFormEmail)
-                /*        localStorage.setItem('registerFormUserPhone', registerFormUserPhone)
-                        localStorage.setItem('registerFormUserEmail', registerFormUserEmail)*/
-                window.location.href = './profileUser.html'
-            }else{
-                alert('error')
-            }
         })
     }
 })
--- src/js/user.js
+++ src/js/user.js
@@ -1,6 +1,9 @@
 const username = localStorage.getItem('loginFormEmail');
+const registerFormUserPhone = localStorage.getItem('registerFormUserPhone');
+const registerFormUserEmail = localStorage.getItem('registerFormUserEmail');
 if (username) {
     document.getElementById('loginFormEmail').textContent = username;
+    document.getElementById('registerFormUserPhone').textContent = registerFormUserPhone;
 } else {
    /* window.location.href = 'index.html';*/
     console.log('error')
