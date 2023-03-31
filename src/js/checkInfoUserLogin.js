064556aac6280dfb2c55c392a8a4b6be8fe8bcb9
Kolya <nk6476066@gmail.com>
2023-03-31 11:58:03 +0300

    user profile change file checkInfoUserLogin.js

--- src/js/checkInfoUserLogin.js
+++ src/js/checkInfoUserLogin.js
@@ -1,47 +1,10 @@
 const checkInfoUserLogin = document.querySelector('.checkInfoUserLogin');
 
 checkInfoUserLogin.addEventListener('submit', (event) => {
-    event.preventDefault()
+    /*event.preventDefault()*/
     const login = checkInfoUserLogin.elements.login.value;
     const password = checkInfoUserLogin.elements.password.value;
     if (login === 'admin@gmail.com' && password === 'admin') {
         window.location.href = 'adminPage/Show.html';
-    } else {
-        const loginForm = document.querySelector('.login_form')
-        loginForm.addEventListener('submit', async (event) => {
-            event.preventDefault()
-            const email = document.querySelector('.login__form-email').value;
-            const password = document.querySelector('.login__form-password').value;
-            const response = await fetch("http://localhost:3000/users?email=" + email)
-            const users = await response.json()
-            if (users.length > 0 && users[0].password === password) {
-                localStorage.setItem('user', JSON.stringify(users[0]));
-                window.location.href = './profileUser.html';
-            } else {
-                alert('Invalid email or password')
-            }
-        })
-        const registerForm = document.querySelector('.register__form');
-        registerForm.addEventListener('submit', async (e)=>{
-            e.preventDefault();
-            const name = document.querySelector('.register__form-user-name').value;
-            const email = document.querySelector('.register__form-email').value;
-            const phone = document.querySelector('.register__form-tel').value;
-            const password = document.querySelector('.register__form-password').value;
-            const newUser = {name, email, phone, password};
-
-            const response = await fetch('http://localhost:3000/users', {
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify(newUser),
-            });
-            if(response.ok){
-                localStorage.setItem('user', JSON.stringify(newUser));
-                window.location.href = './profileUser.html';
-            }else{
-                alert('Error to register')
-            }
-
-        })
     }
 })
---
+++ src/js/form.js
@@ -0,0 +1,39 @@
+const registerForm = document.querySelector('.register__form');
+console.log('registerForm',registerForm)
+registerForm.addEventListener('submit', async (e)=>{
+
+    e.preventDefault();
+    const name = document.querySelector('.register__form-user-name').value;
+    const email = document.querySelector('.register__form-email').value;
+    const phone = document.querySelector('.register__form-tel').value;
+    const password = document.querySelector('.register__form-password').value;
+    const newUser = {name, email, phone, password};
+
+    const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(newUser),
+    });
+    if(response.ok){
+        localStorage.setItem('user', JSON.stringify(newUser));
+        window.location.href = './profileUser.html';
+    }else{
+        alert('Error to register')
+    }
+
+})
+
+const loginForm = document.getElementById('login_form')
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const email = document.querySelector('.login__form-email').value;
+    const password = document.querySelector('.login__form-password').value;
+    const response = await fetch("http://localhost:3000/users?email=" + email)
+    const users = await response.json()
+    if (users.length > 0 && users[0].password === password) {
+        localStorage.setItem('user', JSON.stringify(users[0]));
+        window.location.href = './profileUser.html';
+    } else {
+        alert('Invalid email or password')
+    }
+})
